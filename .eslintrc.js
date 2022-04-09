f557113597a6d908f387b219a0489dfabef8c206
Jesus Lencina <jesusignaciolencina@gmail.com>
2022-04-09 12:21:30 -0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "react/react-in-jsx-scope": 0,
+        "react/prop-types": 0,
+    },
+    settings: {
+        react: {
+            version: "17",
+        },
+    },
+};
