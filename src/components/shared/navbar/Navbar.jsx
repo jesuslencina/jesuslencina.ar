0834751a734ce5a3f60679a636180b71080b7da8
Jesus Lencina <jesusignaciolencina@gmail.com>
2022-06-25 18:15:26 -0300

    added react helmet + trick

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState, useRef } from "react";
+import { Helmet } from "react-helmet";
 import { Link as ScrollLink } from "react-scroll";
 import {
     ArtistPalette,
@@ -13,7 +14,6 @@ import { AiFillCaretDown } from "react-icons/ai";
 import { Context } from "../../../service/Context";
 import NavbarStyled from "./Navbar.styled";
 import { texts } from "./Navbar.texts.json";
-import { defaultColors } from "../../../utils/defaultColors";
 
 const Navbar = () => {
     const { language, setLanguage, setColor } = useContext(Context);
@@ -29,112 +29,119 @@ const Navbar = () => {
     const switchLangSwitcher = () => setLangSwitcherOpen(!langSwitcherOpen);
 
     return (
-        <NavbarStyled>
-            <div className="logo" onClick={() => colorInput.current.click()}>
-                <ArtistPalette size={1.6} rem />
-                <p>{texts[language].myName}</p>
-            </div>
-
-            <input
-                type="color"
-                ref={colorInput}
-                className="hidden-color-switcher"
-                onInput={(e) => setColor(e.target.value)}
-            />
-
-            <nav>
-                <div onClick={switchOpen} className="menu">
-                    {!open ? (
-                        <RiMenu3Line size={26} className="bars" />
-                    ) : (
-                        <VscChromeClose size={26} className="cross" />
-                    )}
+        <>
+            <Helmet>
+                <title>{texts[language].myName}</title>
+            </Helmet>
+            <NavbarStyled>
+                <div
+                    className="logo"
+                    onClick={() => colorInput.current.click()}>
+                    <ArtistPalette size={1.6} rem />
+                    <p>{texts[language].myName}</p>
                 </div>
 
-                <ul className={open ? "active" : null}>
-                    <li>
-                        <ScrollLink smooth to="about">
-                            {texts[language].about}
-                        </ScrollLink>
-                    </li>
+                <input
+                    type="color"
+                    ref={colorInput}
+                    className="hidden-color-switcher"
+                    onInput={(e) => setColor(e.target.value)}
+                />
 
-                    <li>
-                        <ScrollLink smooth to="projects">
-                            {texts[language].projects}
-                        </ScrollLink>
-                    </li>
+                <nav>
+                    <div onClick={switchOpen} className="menu">
+                        {!open ? (
+                            <RiMenu3Line size={26} className="bars" />
+                        ) : (
+                            <VscChromeClose size={26} className="cross" />
+                        )}
+                    </div>
 
-                    <li>
-                        <ScrollLink smooth to="experience">
-                            {texts[language].experience}
-                        </ScrollLink>
-                    </li>
+                    <ul className={open ? "active" : null}>
+                        <li>
+                            <ScrollLink smooth to="about">
+                                {texts[language].about}
+                            </ScrollLink>
+                        </li>
 
-                    <li>
-                        <ScrollLink smooth to="connect">
-                            {texts[language].connect}
-                        </ScrollLink>
-                    </li>
+                        <li>
+                            <ScrollLink smooth to="projects">
+                                {texts[language].projects}
+                            </ScrollLink>
+                        </li>
 
-                    <li className="switcher-area">
-                        <div className="lang-switcher">
-                            <div
-                                className={`flag-container ${
-                                    langSwitcherOpen && "active"
-                                }`}
-                                onClick={switchLangSwitcher}>
-                                {language === "en" ? (
-                                    <FlagUnitedStates size="30" />
-                                ) : language === "es" ? (
-                                    <FlagArgentina size="30" />
-                                ) : (
-                                    <FlagFrance size="30" />
-                                )}
+                        <li>
+                            <ScrollLink smooth to="experience">
+                                {texts[language].experience}
+                            </ScrollLink>
+                        </li>
 
-                                <AiFillCaretDown
-                                    size="14"
-                                    className="triangle"
-                                />
-                            </div>
-                            <div
-                                className={`select-area ${
-                                    langSwitcherOpen && "active"
-                                }`}>
+                        <li>
+                            <ScrollLink smooth to="connect">
+                                {texts[language].connect}
+                            </ScrollLink>
+                        </li>
+
+                        <li className="switcher-area">
+                            <div className="lang-switcher">
                                 <div
-                                    className={` flag-wrapper ${
-                                        language === "es" && "active"
+                                    className={`flag-container ${
+                                        langSwitcherOpen && "active"
                                     }`}
-                                    onClick={() => setLanguage("es")}>
-                                    <FlagArgentina
-                                        size="30"
-                                        className={
-                                            language === "es" && "active"
-                                        }
-                                        onClick={() => setLanguage("es")}
+                                    onClick={switchLangSwitcher}>
+                                    {language === "en" ? (
+                                        <FlagUnitedStates size="30" />
+                                    ) : language === "es" ? (
+                                        <FlagArgentina size="30" />
+                                    ) : (
+                                        <FlagFrance size="30" />
+                                    )}
+
+                                    <AiFillCaretDown
+                                        size="14"
+                                        className="triangle"
                                     />
                                 </div>
-
                                 <div
-                                    className={` flag-wrapper ${
-                                        language === "en" && "active"
-                                    }`}
-                                    onClick={() => setLanguage("en")}>
-                                    <FlagUnitedStates size="30" />
-                                </div>
+                                    className={`select-area ${
+                                        langSwitcherOpen && "active"
+                                    }`}>
+                                    <div
+                                        className={` flag-wrapper ${
+                                            language === "es" && "active"
+                                        }`}
+                                        onClick={() => setLanguage("es")}>
+                                        <FlagArgentina
+                                            size="30"
+                                            className={
+                                                language === "es" && "active"
+                                            }
+                                            onClick={() => setLanguage("es")}
+                                        />
+                                    </div>
 
-                                <div
-                                    className={` flag-wrapper ${
-                                        language === "fr" && "active"
-                                    }`}
-                                    onClick={() => setLanguage("fr")}>
-                                    <FlagFrance size="30" />
+                                    <div
+                                        className={` flag-wrapper ${
+                                            language === "en" && "active"
+                                        }`}
+                                        onClick={() => setLanguage("en")}>
+                                        <FlagUnitedStates size="30" />
+                                    </div>
+
+                                    <div
+                                        className={` flag-wrapper ${
+                                            language === "fr" && "active"
+                                        }`}
+                                        onClick={() => setLanguage("fr")}>
+                                        <FlagFrance size="30" />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </NavbarStyled>
+                        </li>
+                    </ul>
+                </nav>
+            </NavbarStyled>{" "}
+        </>
     );
 };
 
